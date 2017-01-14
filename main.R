bcc3f1f2062877c31688fe87db9fa2bbd79095e1
francisco2001 <francisco2001@hotmail.com>
2017-01-14 13:15:23 +0100

    Some correction in main.R I changed some parts in the main.R, the script works fine!

--- main.R
+++ main.R
@@ -11,6 +11,11 @@ library(sp)
 library(raster)
 library(rgdal)
 
+#Sources. Calling the functions
+
+source("R/cloud2NA.R") # Source de function saved in R folder of the project
+source("R/ndviCalc.R")
+
 
 download.file("https://www.dropbox.com/s/akb9oyye3ee92h3/LT51980241990098-SC20150107121947.tar.gz?dl=1", "data/landsat5.tar.gz", method= "auto",mode="wb")
 download.file("https://www.dropbox.com/s/i1ylsft80ox6a32/LC81970242014109-SC20141230042441.tar.gz?dl=1", "data/landsat8.tar.gz", method= "auto",mode="wb")
@@ -20,7 +25,7 @@ untar('data/landsat5.tar.gz', exdir = "data/")
 untar('data/landsat8.tar.gz', exdir = "data/")
 
 # creating stacks for Landsat data
-list_ls <- list.files('data/', pattern = '*.tif', full.names = TRUE)
+list_ls <- list.files('data/', pattern = glob2rx('*.tif'), full.names = TRUE)
 list_ls
 lands5 <- stack(list_ls[10:24])
 lands8 <- stack(list_ls[1:9])
@@ -37,26 +42,21 @@ fmask5 <- lands5_ext[[1]]
 fmask8 <- lands8_ext[[1]]
 
 # Remove cloud mask (fmask) layer from the Landsat stack
-lands5_NoCloud <- dropLayer(lands5_ext, 1)
-lands8_NoCloud <- dropLayer(lands8_ext, 1)
+lands5_NoCloudmask <- dropLayer(lands5_ext, 1)
+lands8_NoCloudmask <- dropLayer(lands8_ext, 1)
 
-## Replace 'clear land' with 'NA'
-#fmask5[clmask5 == 0] <- NA
-#fmask8[clmask8 == 0] <- NA
-
-source("R/cloud2NA.R") # Source de function saved in R folder of the project
 
 ## Apply the function on the two raster objects using overlay
 
-lands5_CloudFree <- overlay(x = lands5_NoCloud, y = fmask5, fun = cloud2NA)
-lands8_CloudFree <- overlay(x = lands8_NoCloud, y = fmask8, fun = cloud2NA)
+lands5_CloudFree <- overlay(x = lands5_NoCloudmask, y = fmask5, fun = cloud2NA)
+lands8_CloudFree <- overlay(x = lands8_NoCloudmask, y = fmask8, fun = cloud2NA)
 
 names (lands5_CloudFree) = names (lands5_NoCloud) # To recover the original names 
 names (lands8_CloudFree) = names (lands8_NoCloud) # To recover the original names
 
 
 # NDVI calculations
-source("R/ndviCalc.R")
+
 
 ndvilands5 <- overlay(x= lands5_CloudFree[[5]], y=lands5_CloudFree[[6]], fun=ndviCalc)
 plot(ndvilands5, main="NDVI for Landsat TM 5, 1990")
